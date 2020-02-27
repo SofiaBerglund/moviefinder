ead168fec66e3e9bd89f5c01fa77c1b059756103
Sofia Berglund <sofia.berglund@visualart.com>
2020-02-27 15:42:29 +0100

    more styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,28 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Title = styled.h2`
-    margin: 0;
-    color: lightblue;
-`
+  margin: 0;
+`;
+
+const HeaderWrapper = styled.header`
+  background-color: #282c34;
+  height: 70px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  padding: 20px;
+  cursor: pointer;
+`;
 
 const Header = props => {
   return (
-    <header className="App-header">
+    <HeaderWrapper>
       <Title>{props.text}</Title>
-    </header>
+    </HeaderWrapper>
   );
 };
 
